d2c3174e299f59c70a68c86fb2a07573f6f19e43
Joshua King <jarf1337@gmail.com>
2021-01-17 15:10:50 -0500

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd563af3e03db4ae71476854535d7046f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bee19b3d-eea1-417b-af51-8ec667060248'
+};
